f2113d1cedafdc5a6bff486da8b8f1618bb4f391
sean <itseanzhong@gmail.com>
2019-01-01 20:46:31 +0800

    商品服务创建

---
+++ commodity-service/src/main/java/com/zzz/smo/commodityservice/client/UserClient.java
@@ -0,0 +1,14 @@
+package com.zzz.smo.commodityservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Author: Sean
+ * @Date: 2018/12/27 19:18
+ */
+@FeignClient(name = "user-service")
+public interface UserClient  {
+    @GetMapping("/")
+    String getUser();
+}
